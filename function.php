663788235035a03e761abc4be74e23eaed7819bd
AzrielRegi <azrielregi14@gmail.com>
2022-07-20 20:58:32 +0700

    update pelanggan

--- function.php
+++ function.php
@@ -32,23 +32,26 @@ if (isset($_POST['login'])) {
     }
 }
 
-if (isset($_POST['tambahpelanggan'])){
-    //initial variable
+if (isset($_POST['tambahpelanggan'])) {
+    // initial variable
     $nama_pelanggan = $_POST['nama_pelanggan'];
-    $no_tlp = $_POST['no_tlp'];
+    $no_telp = $_POST['no_telp'];
     $alamat = $_POST['alamat'];
 
-    $insertpelanggan = mysqli_query($koneksi, "INSERT INTO pelanggan (nama_pelanggan, no_tlp, alamat) VALUES ('$nama_pelanggan', '$no_tlp', '$alamat')") ;
+    $tambahpelanggan = mysqli_query(
+        $koneksi,
+        "INSERT INTO pelanggan (nama_pelanggan, no_telp, alamat) 
+   VALUES ('$nama_pelanggan','$no_telp','$alamat')"
+    );
 
-    if ($insertpelanggan) {
-        //kalau sukses
+    if ($tambahpelanggan) {
+        // kalau sukses
         header('location:pelanggan.php');
     } else {
-        echo '
-        <script>
-        alert("Gagal tambah pelanggan")
-        window.location.href="pelanggan.php"
-        </script>';
+        echo '<script>
+    alert("Gagal Tambag Pelanggan")
+    window.location.href="pelanggan.php"
+    </script>';
     }
 }
 
@@ -68,47 +71,84 @@ if (isset($_POST['hapuspelanggan'])) {
     }
 }
 
+//edit pelanggan
 if (isset($_POST['editpelanggan'])) {
-     //initial variable
-     $nama_pelanggan = $_POST['nama_pelanggan'];
-     $no_tlp = $_POST['no_tlp'];
-     $alamat = $_POST['alamat'];
-     $id_pelanggan = $_POST['id_pelanggan'];
 
-     $editpelanggan = mysqli_query($koneksi, "UPDATE pelanggan SET 
-     nama_pelanggan='$nama_pelanggan', 
-     no_tlp=$no_tlp, 
-     alamat='$alamat' 
-     WHERE id_pelanggan='$id_pelanggan'");
+    $nama_pelanggan = $_POST['nama_pelanggan'];
+    $notelp = $_POST['no_telp'];
+    $alamat = $_POST['alamat'];
+    $id_pelanggan = $_POST['id_pelanggan'];
 
+    $edit_pelanggan = mysqli_query($koneksi, "UPDATE pelanggan SET nama_pelanggan='$nama_pelanggan', no_telp='$notelp', alamat='$alamat' WHERE id_pelanggan='$id_pelanggan'");
 
-if ($editpelanggan) {
-    // kalau sukses
-    header('location:pelanggan.php');
-} else {
-    echo '<script> 
-    alert("Gagal Edit Pelanggan")
-    window.location.href="pelanggan.php"
-    </script>';
+    if ($edit_pelanggan) {
+        header('location:pelanggan.php');
+    } else {
+        echo '
+        <script>
+        alert("Gagal Edit Pelanggan")
+        window.location.href="pelanggan.php"
+        </script>';
     }
 }
-if (isset($_POST['tambahbarang'])){
-    //initial variable
+
+if (isset($_POST['tambahproduk'])) {
+    //deskripsi initial variable
     $nama_produk = $_POST['nama_produk'];
     $deskripsi = $_POST['deskripsi'];
     $harga = $_POST['harga'];
     $stock = $_POST['stock'];
 
-    $insert_produk = mysqli_query($koneksi, "INSERT INTO produk (nama_produk, deskripsi, harga, stock) 
-    VALUES ('$nama_produk', '$deskripsi', '$harga', '$stock')") ;
+    $tambahproduk = mysqli_query(
+        $koneksi,
+        "INSERT INTO produk (nama_produk, deskripsi, harga, stock) 
+   VALUES ('$nama_produk','$deskripsi','$harga', $stock)"
+    );
 
-    if ($insert_produk) {
-        //kalau sukses
+    if ($tambahproduk) {
+        // kalau sukses
+        header('location:stock.php');
+    } else {
+        echo '<script>
+    alert("Gagal Tambah Produk")
+    window.location.href="stock.php"
+    </script>';
+    }
+}
+if (isset($_POST['editproduk'])) {
+
+    $nama_produk = $_POST['nama_produk'];
+    $deskripsi = $_POST['deskripsi'];
+    $harga = $_POST['harga'];
+    $stock = $_POST['stock'];
+    $id_produk = $_POST['id_produk'];
+
+    $edit_produk = mysqli_query($koneksi, "UPDATE produk SET nama_produk='$nama_produk', 
+                                deskripsi='$deskripsi', 
+                                harga='$harga',
+                                stock='$stock' WHERE id_produk='$id_produk'");
+
+    if ($edit_produk) {
         header('location:stock.php');
     } else {
         echo '
         <script>
-        alert("Gagal tambah barang")
+        alert("Gagal Edit Stock")
+        window.location.href="stock.php"
+        </script>';
+    }
+}
+if (isset($_POST['hapusproduk'])) {
+    $id_produk = $_POST['id_produk'];
+
+    $hapusproduk = mysqli_query($koneksi, "DELETE FROM produk WHERE id_produk='$id_produk'");
+
+    if ($hapusproduk) {
+        // kalau sukses
+        header('location:stock.php');
+    } else {
+        echo '<script> 
+        alert("Gagal Hapus Stock")
         window.location.href="stock.php"
         </script>';
     }
